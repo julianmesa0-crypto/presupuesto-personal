0e302815a105b3482f6a8755c146048b20d14bcf
julianmesa0-crypto <julianmesa0@gmail.com>
2026-09-21 13:58:53 -0500

    Update Presupuesto.py

--- Presupuesto.py
+++ Presupuesto.py
@@ -16,7 +16,7 @@ import io
 # 1. CONFIGURACIÓN DE PÁGINA
 # ==========================================
 st.set_page_config(
-    page_title="Presupuesto 50/30/20 Plus",
+    page_title="OptiBudget Pro",
     page_icon="💼",
     layout="wide",
     initial_sidebar_state="expanded"
